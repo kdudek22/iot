b985933c51f90e10a2a1edee8e1c10f703c3b6b5
omnibus1 <appleofeden8686@gmail.com>
2024-05-20 14:02:30 +0200

    slight refactor

--- backend/src/main/java/com/example/demo/EventHandler/EventHandler.java
+++ backend/src/main/java/com/example/demo/EventHandler/EventHandler.java
@@ -32,23 +32,17 @@ public class EventHandler {
         }
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SS");
 
-
-
         String eventData = String.valueOf(event.getData()).replace("\\","");
 
         LocalDateTime postDateTime = LocalDateTime.parse(event.getEventTime().toString().substring(0,22));
-//        postDateTime = postDateTime.plusHours(1);
-//        String formatted_date = formatter.format(postDateTime);
-//        postDateTime = LocalDateTime.parse(formatted_date);
-        System.out.println("---------");
-        System.out.println(postDateTime);
-        System.out.println("---------");
+
         HashMap<String, String> map = new Gson().fromJson(eventData.substring(1,eventData.length()-1), HashMap.class);
 
         Device device = deviceRepository.getDeviceBySn(map.get("sn"));
         if(device==null){
             System.out.println("not found serial number: "+ map.get("sn"));
         }
+
         Reading reading = new Reading(null, device, map.get("value"), postDateTime);
         readingRepository.saveReading(reading);
 
